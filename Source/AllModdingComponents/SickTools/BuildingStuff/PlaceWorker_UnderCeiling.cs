4166dabf46d1d9611920a69fa7c5775c5e2a5380
Johnny Hastings <johnny.hastings@gmail.com>
2024-04-13 07:59:20 -0500

    Moved in PlaceWorker for UnderRoof

---
+++ Source/AllModdingComponents/SickTools/BuildingStuff/PlaceWorker_UnderCeiling.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using RimWorld;
+using Verse;
+
+namespace SickTools
+{
+    public class PlaceWorker_UnderCeiling : PlaceWorker
+    {
+        public override AcceptanceReport AllowsPlacing(
+          BuildableDef checkingDef,
+          IntVec3 loc,
+          Rot4 rot,
+          Map map,
+          Thing thingToIgnore = null,
+          Thing thing = null)
+        {
+            if (!map.roofGrid.Roofed(loc))
+                return new AcceptanceReport("ST_PlaceWorker_UnderCeiling".Translate());
+            return (AcceptanceReport)true;
+        }
+    }
+}
